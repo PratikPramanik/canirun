04532485a0d14be7aeb48d8df74d300cc5f60f17
Pratik Pramanik <pratikpramanik@gmail.com>
2012-02-19 21:08:38 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canirun::Application.config.secret_token = 'e5383fcff844548fdc6002df37c1c6b8b1746bec5227b2d3306845d17f466c0ad2849ddcc7b76d05a8f5b4bab4f90ed7fc3959e79ceac5560e84a2c326059514'
